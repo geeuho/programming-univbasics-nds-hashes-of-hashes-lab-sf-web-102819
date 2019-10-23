6c1c3dc6d22f3f47c56f56cb55e8f511afff05f4
Allen Shin <geeuho@gmail.com>
2019-10-23 00:59:35 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,9 +46,7 @@ def naming_system
             label: "Genus",
             sub_category: {
               label: "Species"
-              sub_category: {
-                nil
-              }
+              sub_category: nil
             }
           }
         }
