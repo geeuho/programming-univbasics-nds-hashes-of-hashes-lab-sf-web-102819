a917f7599eccab6fd86af55e16ae148a891d23f3
Allen Shin <geeuho@gmail.com>
2019-10-23 01:03:43 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,7 +32,7 @@
 
 def naming_system
   
-Animal_kingdom = 
+{
   label: "Kingdom",
   sub_category: {
     label: "Phylum",
@@ -53,7 +53,7 @@ Animal_kingdom =
       }
     }
   }
-
+}
   
   # Remember:
   #  Kingdom
